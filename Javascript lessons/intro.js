544469bbe5d833df3bd878ca5e1353b02d38a3e5
Sreethu K Binu <101237839+sreethukb@users.noreply.github.com>
2024-06-01 01:19:07 +0530

    Add files via upload

---
+++ Javascript lessons/intro.js
@@ -0,0 +1,9 @@
+alert("Hello"); //will generate a popup with msg "Hello"
+//it is short for window.alert("Message");
+
+prompt("What is your name?"); //will generate a prompt popup with textarea to take input from user along with mentioned message
+
+//variables
+//use camelCasing
+var myName = "Sreethu";
+var yourName = prompt("Enter your name:"); //will take input from the prompt and store in the variable yourName.
