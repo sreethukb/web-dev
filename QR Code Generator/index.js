a9a67ea350f9b059ddfccc9d6cab566d1108ff8f
Sreethu K Binu <101237839+sreethukb@users.noreply.github.com>
2024-07-30 16:53:50 +0530

    Add files via upload

--- QR Code Generator/index.js
+++ QR Code Generator/index.js
@@ -1,32 +1,23 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
-
-import inquirer from 'inquirer';
-import qr from 'qr-image';
-import fs from 'fs';
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
 
 inquirer
-    .prompt([{
-        message: "Enter the URL: ",
-        name: "URL",
-    },])
-    .then((answers) => {
-        const url = answers.URL;
-        var qr_svg = qr.image(url);
-        qr_svg.pipe(fs.createWriteStream('QR code.png'));
-        fs.writeFile("url.txt", url, (err) =>{
-            if(err) throw err;
-            console.log("QR code generated and URL saved successfully.");
-        });
-    })
-    .catch((error) => {
-        if(error.isTtyError) {
-            //prompt couldn't be rendered in the current environment
-        } else{
-            //something else went wrong
-        }
-    });
-
+  .prompt([
+    {
+        message:"Enter URL here:",
+        name:"URL"
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url, { type: 'png' });
+    qr_svg.pipe(fs.createWriteStream('linkedin.png'));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
